0fb2db34f6ccc324b5a7678de35437714f77c982
Jasan Hernandez <jasan_azael@hotmail.es>
2021-02-23 16:49:20 -0600

    add casino practice

---
+++ README.md
@@ -0,0 +1,3 @@
+## Prácticas de C / C++
+
+En este repositorio se estaran subiendo programas de consola, creados con el lenguaje C o C++, con buenas prácticas de POO.
---
+++ index.cpp
@@ -0,0 +1,141 @@
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
+#include <string>
+#include <wchar.h>
+#include <locale.h>
+
+using namespace std;
+
+class Casino {
+  private:
+    int balance;
+    int betAmount;
+    int selectNumber;
+    string name;
+    void showMenu();
+    void toBet();
+    void chooseNumber();
+    void compareNumbers(int);
+    bool setBetAmount(int);
+    bool setSelectNumber(int);
+    int rollDice(int, int);
+  public:
+    void runGame();
+    int getBalance();
+    Casino(int);
+};
+
+int Casino::getBalance() {
+  return this->balance;
+}
+
+Casino::Casino(int balance) {
+  this->balance = balance;
+}
+
+bool Casino::setBetAmount(int betAmount) {
+  if(betAmount <= this->balance) {
+    if(betAmount <= 0) {
+      cout << "La apuesta no debe ser menor o igual que 0 \n";
+      return false;
+    } 
+    this->betAmount = betAmount;
+    return true;
+  } 
+  cout << "Saldo insuficiente :( \n";
+  return false;
+}
+
+bool Casino::setSelectNumber(int number) {
+  if(number <= 6) {
+    if(number <= 0) {
+      cout << "El número seleccionado no debe ser menor que 0 \n";
+      return false;
+    }
+    this->selectNumber = number;
+    return true;
+  }
+  cout << "El número seleccionado no debe ser mayor que 6 \n";
+  return false;
+}
+
+int Casino::rollDice(int from, int to) {
+  srand(time(NULL));
+  int random = rand()%(to - from+1)+from;
+  return random;
+}
+
+void Casino::compareNumbers(int randomNumber) {
+  if(randomNumber == this->selectNumber) {
+    cout << "Excelente :) \n";
+    cout << "El número aletorio es " << randomNumber << " y el número selecionado es: " << this->selectNumber << " " << "por lo tanto es empate \n";
+  } else if(randomNumber % 2 != 0 && this->selectNumber % 2 != 0) {
+     cout << "Excelente :) \n";
+     cout << "El número aletorio es " << randomNumber << " y el número selecionado es: " << this->selectNumber;
+     cout << "\nAmbos Números son impares por lo tanto estó es un empate \n";
+  }else {
+    this->balance = this->balance - this->betAmount;
+    cout << "Upps: :( No tuvó suerte en esté tiro \n";
+    cout << "El número aletorio es " << randomNumber << " y el número selecionado es: " << this->selectNumber << " \n";
+    cout << this->name << " usted acaba de perder " << this->betAmount << " PESOS MX \n";
+    cout << "Su saldo actual ahora es de $" << this->balance << " PESOS MX \n";
+  }
+}
+
+void Casino::showMenu() {
+  cout << ":::: Bienvenido al Casino " << this->name <<  " :::: \n\n";
+  cout << "Descripción del Juego: \n";
+  cout << "Se Tira un dado entre 1 y 6 y usted deberá adivinar que número cayo \n\n";
+  cout << "**Reglas** \n";
+  cout << "1. Usted tiene la cantidad inicial de " << this->balance << " PESOS MX \n";
+  cout << "2. Usted es capaz de apostar una cantidad que no sea 0 y tampoco sea mayor a su saldo actual \n";
+  cout << "3. Si Usted no le atina al número que cayo pierde su apuesta \n";
+  cout << "4. Si usted le atina al número que cayó se considerá empaté \n";
+  cout << "5. Si el número que escribió es impar y el número que cayo es impar, se considerá empate \n";
+  cout << "6. Si usted no le atina al número que cayo pierde su apuesta \n";
+  cout << "7. Si el número que cayo es par y el que escribió es impar, pierde su apuesta \n";
+  cout << "8: El juego terminará cuando su saldo esté en 0 \n";
+}
+
+void Casino::toBet() {
+  int apuesta;
+  cout << "Ingresa la cantidad que deseas apostar: ";
+  cin >> apuesta;
+  bool isCorrect = this->setBetAmount(apuesta);
+  if(isCorrect == false) {
+  	this->toBet();
+  }
+}
+
+void Casino::chooseNumber() {
+  int selectNumber;
+  cout << "Elige un número del 1 al 6: ";
+  cin >> selectNumber;
+  bool isCorrect = this->setSelectNumber(selectNumber);
+  if(isCorrect == false) {
+    this->chooseNumber();
+  }
+}
+
+
+void Casino::runGame() {
+  cout << "Escribe tu nombre antes de comenzar: ";
+  getline(cin, this->name);
+  this->showMenu();
+  while (this->getBalance()) {
+    this->toBet();
+    this->chooseNumber();
+    int random = this->rollDice(1, 6);
+    this->compareNumbers(random);
+  }
+  
+}
+
+int main() {
+  setlocale(LC_ALL, "");
+  Casino casino = Casino(500);
+  casino.runGame();
+  system("pause");
+  return 0;
+}
